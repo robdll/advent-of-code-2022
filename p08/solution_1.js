3bbab778e2970ab0b2f302c7977664630307317d
robdll <dilillo.roberto@gmail.com>
2022-12-08 20:05:43 -0500

    adds day 8 solutions

---
+++ p08/solution_1.js
@@ -0,0 +1,85 @@
+const { Resolver } = require('dns');
+const fs = require('fs');
+const filename = './input.txt';
+
+
+// generate a function to read a file, and split it into lines, then convert each line into an array of numbers
+const patch = fs
+        .readFileSync(filename, 'utf-8')
+        .split(/\r?\n/)
+        .map((line) => line
+            .split('')
+            .map( i => parseInt(i))
+            .map( i => ({ value: i, visible: false}))
+        )
+const patchHeight = patch.length;
+const patchWidth = patch[0].length;
+
+let maximals = {
+    top: [],
+    left: 0,
+    bottom: [],
+    right: 0,
+};
+let totalVisibles = 0;
+
+for(let i = 0; i < patchHeight; i++){
+    for(let j = 0; j < patchWidth; j++){
+        const currentTree = patch[i][j]
+        // top line is always visible and are the maximal for north to south
+        if(i === 0) {
+            maximals.top.push(currentTree.value);
+            currentTree.visible = true
+        } 
+        // first element of each line is always visible and is maximal for west to east
+        if(j === 0) {
+            maximals.left = currentTree.value;
+            currentTree.visible = true
+        } 
+        // any other elements will be visible if can be visible from west to east or from north to south,
+        if(!currentTree.visible) {
+            currentTree.visible = currentTree.value > maximals.left || currentTree.value > maximals.top[j];
+        }
+        // update maximal if needed
+        maximals.left = Math.max(maximals.left, currentTree.value);
+        maximals.top[j] = Math.max(maximals.top[j], currentTree.value);
+    }
+}
+
+for(let i = patchHeight -1; i >= 0; i--) {
+    if(i === patchHeight -2) {
+        maximals.bottom = maximals.bottom.reverse();
+    }
+    for(let j = patchWidth -1; j >= 0; j--){
+        const currentTree = patch[i][j];
+        // bottom line is always visible and each element is the maximal for south to north
+        if(i === patchHeight -1) {
+            maximals.bottom.push(currentTree.value);
+            currentTree.visible = true
+        }
+        // rightmost element of each line is always visible and is maximal for west to east
+        if(j === patchWidth -1) {
+            maximals.right = currentTree.value;
+            currentTree.visible = true
+        }
+        // any other elements will be visible if can be visible from east to west or from south to north
+        if(!currentTree.visible) {
+            currentTree.visible = currentTree.value > maximals.right || currentTree.value > maximals.bottom[j];
+        }
+        if(i !== patchHeight -1){
+            maximals.bottom[j] = Math.max(maximals.bottom[j], currentTree.value);
+        }
+        maximals.right = Math.max(maximals.right, currentTree.value);
+    }
+}
+
+
+ for(let i = 0; i < patchHeight; i++) {
+     for(let j = 0; j < patchWidth; j++) {
+         if(patch[i][j].visible) {
+             totalVisibles++;
+         }
+     }
+ }
+
+console.log(totalVisibles)
---
+++ p08/solution_2.js
@@ -0,0 +1,84 @@
+const { Resolver } = require('dns');
+const fs = require('fs');
+const filename = './input.txt';
+
+// generate a function to read a file, and split it into lines, then convert each line into an array of numbers
+const patch = fs
+        .readFileSync(filename, 'utf-8')
+        .split(/\r?\n/)
+        .map((line) => line
+            .split('')
+            .map( i => parseInt(i))
+            .map( i => ({ value: i, visible: false}))
+        )
+const patchHeight = patch.length;
+const patchWidth = patch[0].length;
+
+const patchClone = []
+for(let i = 0; i < patchHeight; i++){
+    patchClone.push([]);
+    for(let j = 0; j < patchWidth; j++){
+        const currentTree = patch[i][j];
+        const visibleTreesN = countTrees(i, j, 'N');
+        const visibleTreesE = countTrees(i, j, 'E');
+        const visibleTreesS = countTrees(i, j, 'S');
+        const visibleTreesW = countTrees(i, j, 'W');
+        patchClone[i][j] = visibleTreesN * visibleTreesE * visibleTreesS * visibleTreesW
+    }
+}
+
+let highestScenicScore = 0;
+ for(let i = 0; i < patchHeight; i++) {
+     for(let j = 0; j < patchWidth; j++) {
+        highestScenicScore = Math.max(highestScenicScore, patchClone[i][j])
+     }
+ }
+
+console.log(highestScenicScore)
+
+function countTrees(i, j, dir) {
+    let count = 0;
+    switch(dir) {
+        case 'N':
+            for(let k = i - 1; k >= 0; k--) {
+                if(patch[k][j].value <= patch[i][j].value) {
+                    count++;
+                }
+                if(patch[k][j].value >= patch[i][j].value) {
+                    break;
+                }
+            }
+            break;
+        case 'E':
+            for(let k = j + 1; k < patchWidth; k++) {
+                if(patch[i][k].value <= patch[i][j].value) {
+                    count++;
+                }
+                if(patch[i][k].value >= patch[i][j].value) {
+                    break;
+                }
+            }
+            break;
+        case 'S':
+            for(let k = i + 1; k < patchHeight; k++) {
+                if(patch[k][j].value <= patch[i][j].value) {
+                    count++;
+                }
+                if(patch[k][j].value >= patch[i][j].value) {
+                    break;
+                }
+            }
+            break;
+        case 'W':
+            for(let k = j - 1; k >= 0; k--) {
+                if(patch[i][k].value <= patch[i][j].value) {
+                    count++;
+                }
+                if(patch[i][k].value >= patch[i][j].value) {
+                    break;
+                }
+            }
+            break;
+    }
+    return count;
+}
